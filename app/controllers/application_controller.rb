6b0977187d71791e2fa4c724d4549cfa2dbf09c8
Michael Hartl <michael@michaelhartl.com>
2013-07-06 15:18:27 -0700

    Get relationships controller tests to pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SessionsHelper
+
+  # Force signout to prevent CSRF attacks
+  def handle_unverified_request
+    sign_out
+    super
+  end
 end
