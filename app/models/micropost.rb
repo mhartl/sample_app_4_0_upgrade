7ac97ff926fda3cf0918e2fadd802bb0a7f2cea6
Michael Hartl <michael@michaelhartl.com>
2013-07-06 14:08:09 -0700

    Get micropost specs to pass

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,7 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 
-  default_scope order: 'microposts.created_at DESC'
+  default_scope -> { order('microposts.created_at DESC') }
 
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -14,14 +14,6 @@ describe Micropost do
 
   it { should be_valid }
 
-  describe "accessible attributes" do
-    it "should not allow access to user_id" do
-      expect do
-        Micropost.new(user_id: user.id)
-      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
-    end
-  end
-
   describe "when user_id is not present" do
     before { @micropost.user_id = nil }
     it { should_not be_valid }
