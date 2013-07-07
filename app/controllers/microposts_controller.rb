1880afd06a707d7568572fc19679be65bdfb40c4
Michael Hartl <michael@michaelhartl.com>
2013-07-06 17:05:40 -0700

    Update app to use strong parameters

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,7 +3,7 @@ class MicropostsController < ApplicationController
   before_filter :correct_user,   only: :destroy
 
   def create
-    @micropost = current_user.microposts.build(params[:micropost])
+    @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
@@ -19,6 +19,10 @@ class MicropostsController < ApplicationController
   end
 
   private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 
     def correct_user
       @micropost = current_user.microposts.find_by_id(params[:id])
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,4 @@
 class Micropost < ActiveRecord::Base
-  # attr_accessible :content
   belongs_to :user
 
   validates :user_id, presence: true
